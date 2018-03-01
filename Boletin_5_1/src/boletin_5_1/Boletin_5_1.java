ad0598467b772091e5e6a9dcc1756b42edc5ec6f
mfrancomartinez <mfrancomartinez@victoria27.danielcastelao.org>
2018-03-01 08:40:50 +0100

    Boletin_5_1

---
+++ Boletin_5_1/src/boletin_5_1/Boletin_5_1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_5_1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mfrancomartinez
+ */
+public class Boletin_5_1 {
+private static float numero;
+    public static void main(String[] args) {
+Boletin_5_1_class obx1 = new Boletin_5_1_class();
+obx1.positivo_negativo();
+}
+
+    }
+    
