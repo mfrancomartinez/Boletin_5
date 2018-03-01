6ff5a1c56a8e1784ae977d4f681ce18d4243bcb1
mfrancomartinez <mfrancomartinez@victoria27.danielcastelao.org>
2018-03-01 08:42:05 +0100

    Boletin_5_4

---
+++ Boletin_5_4/src/boletin_5_4/Boletin_5_4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin_5_4;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mfrancomartinez
+ */
+public class Boletin_5_4 {
+
+    public static void main(String[] args) {
+Boletin_5_4_class obx = new Boletin_5_4_class();
+obx.compararpeso();
+}
+    }  
+    
+
